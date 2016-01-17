f5fe2e755ead48cc70cb6b295a3d1fbb55930fbe
Sorae <seokmin@nhnnext.org>
2016-01-17 23:50:18 +0900

    암호화키 변경

--- C# ServerWithClient/Network Project/Encrypter.cs
+++ C# ServerWithClient/Network Project/Encrypter.cs
@@ -4,24 +4,77 @@ using System.Text;
 
 public static class Encrypter
 {
-	private static byte[] key = new byte[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
-	private static byte[] iv = new byte[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
+	private static byte[] key = new byte[8] { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88 };
+	private static byte[] iv = new byte[8] { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88 };
 
 	public static string Crypt(this string text)
 	{
-		SymmetricAlgorithm algorithm = DES.Create();
-		ICryptoTransform transform = algorithm.CreateEncryptor(key, iv);
-		byte[] inputbuffer = Encoding.Unicode.GetBytes(text);
-		byte[] outputBuffer = transform.TransformFinalBlock(inputbuffer, 0, inputbuffer.Length);
-		return Convert.ToBase64String(outputBuffer);
+		try
+		{
+			//SymmetricAlgorithm algorithm = DES.Create();
+			DES des = new DESCryptoServiceProvider();
+			des.Mode = CipherMode.ECB;
+			des.Padding = PaddingMode.PKCS7;
+			des.Key = key;
+			des.IV = iv;
+			//ICryptoTransform transform = algorithm.CreateEncryptor(key, iv);
+			ICryptoTransform transform = des.CreateEncryptor(key,iv);
+			byte[] inputbuffer = Encoding.Unicode.GetBytes(text);
+			byte[] outputBuffer;
+			try
+			{
+				outputBuffer = transform.TransformFinalBlock(inputbuffer, 0, inputbuffer.Length);
+			}
+			catch(Exception e)
+			{
+				inputbuffer = Encoding.UTF8.GetBytes(text);
+				outputBuffer = transform.TransformFinalBlock(inputbuffer, 0, inputbuffer.Length);
+			}
+			string asdfs = Convert.ToBase64String(outputBuffer);
+			return asdfs;
+		}
+		catch (Exception e)
+		{
+			return "";
+		}
+
 	}
 
 	public static string Decrypt(this string text)
 	{
-		SymmetricAlgorithm algorithm = DES.Create();
-		ICryptoTransform transform = algorithm.CreateDecryptor(key, iv);
-		byte[] inputbuffer = Convert.FromBase64String(text);
-		byte[] outputBuffer = transform.TransformFinalBlock(inputbuffer, 0, inputbuffer.Length);
-		return Encoding.Unicode.GetString(outputBuffer);
+		try
+		{
+			DES des = new DESCryptoServiceProvider();
+			des.Mode = CipherMode.ECB;
+			des.Padding = PaddingMode.PKCS7;
+			des.Key = key;
+			des.IV = iv;
+			//SymmetricAlgorithm algorithm = DES.Create();
+			//ICryptoTransform transform = algorithm.CreateDecryptor(key, iv);
+			ICryptoTransform transform = des.CreateDecryptor();
+			byte[] inputbuffer = Convert.FromBase64String(text);
+			
+			byte[] outputBuffer = transform.TransformFinalBlock(inputbuffer, 0, inputbuffer.Length);
+			return Encoding.Unicode.GetString(outputBuffer);
+		}
+		catch (Exception e)
+		{
+			byte[] utfBytes = Encoding.UTF8.GetBytes(text);
+
+			byte[] unicodeBytes = Encoding.Convert(Encoding.UTF8, Encoding.Unicode, utfBytes);
+			char[] unicodeChars = new char[Encoding.Unicode.GetCharCount(unicodeBytes, 0, unicodeBytes.Length)];
+			Encoding.Unicode.GetChars(unicodeBytes, 0, unicodeBytes.Length, unicodeChars, 0);
+			text = new string(unicodeChars);
+			DES des = new DESCryptoServiceProvider();
+			des.Mode = CipherMode.ECB;
+			des.Padding = PaddingMode.None;
+			des.Key = key;
+			des.IV = iv;
+			ICryptoTransform transform = des.CreateDecryptor();
+			byte[] inputbuffer = Convert.FromBase64String(text);
+
+			byte[] outputBuffer = transform.TransformFinalBlock(inputbuffer, 0, inputbuffer.Length);
+			return Encoding.Unicode.GetString(outputBuffer);
+		}
 	}
 }
